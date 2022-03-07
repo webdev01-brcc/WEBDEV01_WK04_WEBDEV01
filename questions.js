cd0e82c07e0184b04d3f202855973f8a4d3aeb4c
WEBDEV01 <webdev01@Alfreds-MacBook-Pro.local>
2022-03-07 02:33:01 -0600

    Added the questions.

---
+++ questions.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        type: 'list',
+        name: 'q1',
+        message: 'What is the capital of Louisiana?',
+        choices: [
+            "New Orleans",
+            "Shreveport",
+            "Lafayette",
+            "Baton Rouge"
+        ]
+    },
+    {
+        type: 'list',
+        name: 'q2',
+        message: 'What year did Hurricane Katrina hit New Orleans?',
+        choices: [
+            2003,
+            2005,
+            2006,
+            2010
+        ]
+    },
+    {
+        type: 'list',
+        name: 'q3',
+        message: "What is Louisiana's state bird?",
+        choices: [
+            "Falcon",
+            "Eagle",
+            "Pelican",
+            "Raven"
+        ]
+    },
+    {
+        type: 'list',
+        name: 'q4',
+        message: "What is Louisiana's largest city?",
+        choices: [
+            "Shreveport",
+            "Baton Rouge",
+            "New Orleans",
+            "Hammond"
+        ]
+    },
+    {
+        type: 'list',
+        name: 'q5',
+        message: "What year was the Louisiana Purchase?",
+        choices: [
+            1767,
+            1803,
+            1866,
+            1867
+        ]
+    },
+]
